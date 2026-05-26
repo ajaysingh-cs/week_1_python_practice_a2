9279001f13945acfb9fafc7735ac5c6ea1bc3b30
Ajay Singh <ajayabanna076@gmail.com>
2026-05-27 00:17:40 +0530

    mini project

--- exercise.py
+++
@@ -1,3 +0,0 @@
-
-superhero_name = input("what is your superhero name ?")
-print("your superhero name is " + superhero_name)
--- first.py
+++
@@ -1,4 +0,0 @@
-name="tony stark"
-age=45
-is_genius=True
-print(name + " is " + str(age) + " years old and is a genius: " + str(is_genius))
